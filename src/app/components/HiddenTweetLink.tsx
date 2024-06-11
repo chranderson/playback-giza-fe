c232c78b082da5ca83513abe8e65ec3209940df7
Chris Anderson <chranderson@users.noreply.github.com>
2024-06-11 16:13:41 -0600

    enables hidden link to be clickable

--- src/app/components/HiddenTweetLink.tsx
+++ src/app/components/HiddenTweetLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const HiddenTweetLink = () => {
   return (
     <Link
-      className="absolute invisible"
+      className="!text-background hover:!text-primary"
       aria-label="we are looking for p̶a̶t̶i̶e̶n̶t̶ z̶e̶r̶o̶ our first beta tester. x.com link - @playbacknet"
       href="https://x.com/playbacknet/status/1800562771773493252"
     >
