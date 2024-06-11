dc980631f84479e8198f73cf038bd270574d6bc6
Chris Anderson <chranderson@users.noreply.github.com>
2024-06-11 15:05:31 -0600

    uses invisible class for hidden tweet link

--- src/app/components/HiddenTweetLink.tsx
+++ src/app/components/HiddenTweetLink.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 const HiddenTweetLink = () => {
   return (
     <Link
-      aria-label="x.com link - @playbacknet"
-      className="absolute top-0 left-0 opacity-0 h-0 w-0"
+      className="absolute invisible"
+      aria-label="we are looking for p̶a̶t̶i̶e̶n̶t̶ z̶e̶r̶o̶ our first beta tester. x.com link - @playbacknet"
       href="https://x.com/playbacknet/status/1800562771773493252"
     >
       we are looking for p̶a̶t̶i̶e̶n̶t̶ z̶e̶r̶o̶ our first beta tester.
